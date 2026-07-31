157cacfe3e9178e6306a5cb1fda06d718b6c840e
bonini81 <andresdominguez81@gmail.com>
2026-07-30 20:43:28 -0500

    Criterios page

--- src/components/Criterios/Criterios.jsx
+++ src/components/Criterios/Criterios.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { EVALUATION_CRITERIA } from '../../data/criteria'
+import Button from '../Button/Button'
 import styles from './Criterios.module.css'
 
 // El criterio 8 sigue sin confirmar por el cliente, así que en esta pantalla
@@ -11,21 +12,18 @@ function Criterios() {
 
   return (
     <section className={styles.screen}>
-      <h1 className={styles.title}>Conoce los criterios de evaluación</h1>
-      <p className={styles.intro}>
-        La evaluación será sobre 40 puntos, siendo 1 el puntaje más bajo y 5 el más alto de
-        calificación en cada parámetro.
-      </p>
 
-      <ul className={styles.list}>
-        {VISIBLE_CRITERIA.map((criterion) => (
-          <li key={criterion.id}>{criterion.label}</li>
-        ))}
-      </ul>
+       <article className={styles.logo}>
+                <img src="/src/assets/images/logo-onStar.png" alt="Logo OnStar" />
+              </article>
 
-      <button className={styles.primaryButton} onClick={() => navigate('/registro')}>
-        Entendido
-      </button>
+  
+
+    <div className={styles.ring}>
+      <Button onClick={() => navigate('/registro')}>
+         Entendido
+        </Button>
+   </div>
     </section>
   )
 }
